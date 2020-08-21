5a21cb92543efb4220d823d6c06bd0eaaa94dd8b
Karpova <karpovai@outlook.com>
2020-08-21 20:26:54 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 ;(function() {
 	"use strict";
- 
+	new WOW().init();
+	var mySwiper = new Swiper('.swiper-container', {
+		loop: true,
+		pagination: {
+			el: '.ba-pagination',
+			bulletClass: 'ba-empty',
+			bulletActiveClass: 'ba-active',
+			clickable: true,
+		 },
+	 })
 })();
 
